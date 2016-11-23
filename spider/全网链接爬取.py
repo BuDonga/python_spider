9a1bd6c5eb7eb1c3408f5e3a017864af4dddc662
BuDong <fish880111@163.com>
2016-11-23 17:28:45 +0800

    1

--- spider/全网链接爬取.py
+++ spider/全网链接爬取.py
@@ -3,12 +3,18 @@ import urllib2
 import re
 import time
 import urlparse
+import robotparser
 
 
-def download(url, user_agent='wswp', try_numbers=2):
-    print 'trying %d Downloading...', url
+def download(url, user_agent='wswp', try_numbers=2, proxy=None):
+    print 'trying Downloading...', url
     header = {'user_agent': user_agent}
     request = urllib2.Request(url, headers=header)
+    opener = urllib2.build_opener()
+    if proxy:
+        """支持代理"""
+        proxy_params = {urlparse.urlparse(url).scheme: proxy}
+        opener.add_handler(urllib2.ProxyHandler(proxy_params))
     try:
         html = urllib2.urlopen(request).read()
         time.sleep(1)
@@ -41,21 +47,33 @@ def get_all_links(url):
     return unique_links
 
 
+def robots_check(url, user_agent):
+    """robot.txt检查，避免下载禁止爬取的代理"""
+    robot_path = url + 'robots.txt'
+    rp = robotparser.RobotFileParser()
+    rp.set_url(robot_path)
+    rp.read()
+    return rp.can_fetch(user_agent, url)
+
+
 def crawler(url):
     crawler_link = [url]
     seen_link = set(crawler_link)
     while crawler_link:
         link = crawler_link.pop()
-        all_links = get_all_links(link)
-        for return_link in all_links:
-            if urlparse.urljoin(url, '/') in return_link:  # 去除外网链接
-                if return_link not in seen_link:
-                    seen_link.add(return_link)
-                    crawler_link.append(return_link)
-                    print '\n'
-                    print '*' * 50 + return_link + '*' * 50
-                else:
-                    print '%s is in the list!!!!!!!' % return_link
+        if robots_check(url, user_agent='user_agent'):  # 增加robot.txt检查，避免下载禁止爬取的代理
+            all_links = get_all_links(link)
+            for return_link in all_links:
+                if urlparse.urljoin(url, '/') in return_link:  # 去除外网链接
+                    if return_link not in seen_link:
+                        seen_link.add(return_link)
+                        crawler_link.append(return_link)
+                        print '\n'
+                        print '*' * 50 + return_link + '*' * 50
+                    else:
+                        print '%s is in the list!!!!!!!' % return_link
+        else:
+            print 'Blocked by robots.txt:', url
     print 'running over!!!!!!!!!!!!!!'
 
 
